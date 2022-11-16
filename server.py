2157bc2ec9c09b7f77c411db8c6d011f719e8030
Chandan Singh <csrrocz@gmail.com>
2022-11-16 20:45:40 +0100

    changed the directory of the backend file

---
+++ server.py
@@ -0,0 +1,18 @@
+import json
+from starlette.responses import FileResponse 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+f = open('data.json')
+data = json.load(f)
+
+
+@app.get("/courses")
+def home():
+    return {'data': data}
+
+@app.get("/")
+async def read_index():
+    return FileResponse('index.html')
