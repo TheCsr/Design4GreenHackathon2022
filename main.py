46a9b32d462aa2437e7d93f86d9b3069fac7ca82
Chandan Singh <csrrocz@gmail.com>
2022-11-17 13:37:14 +0100

    sending email functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 from starlette.responses import FileResponse 
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
+import yagmail
 
 app = FastAPI()
 
@@ -12,8 +13,22 @@ data = json.load(f)
 def home():
     return {'data': data}
 
+@app.post("/sendEmail")
+async def sendEmail(msg: str):
+    yag = yagmail.SMTP('hackathon2022team26@gmail.com', 'xtcfgqkknmvleeuj')
+    try:
+        yag.send('csrrocz@gmail.com', 'test', msg)
+        return {"send": "successful"}
+    except:
+        return {"error": "error"}
+        
+
 app.mount("/", StaticFiles(directory="Frontend",html = True), name="Frontend")
 @app.get("/")
 async def read_index():
     return FileResponse('index.html')
+
+
+        
+
 
